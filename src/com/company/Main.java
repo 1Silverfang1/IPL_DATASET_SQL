8cd4d9148cd5f33e96b9124ddd8ba5d63ae0fff3
root <root@localhost.localdomain>
2019-11-26 00:12:15 +0530

    version2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,23 +2,34 @@ package com.company;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.LinkedHashMap;
 
 import static com.company.ConstantValues.*;
 
 public class Main {
+
+    private LinkedHashMap<String,String> QuerySolver(String query,String columnFirst, String columnSecond) throws SQLException {
+        LinkedHashMap<String,String> solvedQuery= new LinkedHashMap<>();
+        getResultSet=getStatement.executeQuery(query);
+        while (getResultSet.next())
+        {
+            String firstColumn = getResultSet.getString(columnFirst);
+            String secondColumn = getResultSet.getString(columnSecond);
+            solvedQuery.put(firstColumn,secondColumn);
+        }
+        return solvedQuery;
+    }
     public static void main(String[] args) {
 
 
         try {
             getConnection = DriverManager.getConnection(jdbcUrlforDatabase, username, password);
             getStatement = getConnection.createStatement();
-            querySolver(FIRST_QUERY, SEASON_COLUMN, MATCH_PLAYED, SECOND_QUERY, TOTAL_WINS);
-            querySolver(THIRD_QUERY, BOWLING_TEAMS, EXTRA_RUNS, FOURTH_QUERY, MATCH_WIN);
-            getResultSet = getStatement.executeQuery(ConstantValues.FIFTH_QUERY);
-            while (getResultSet.next()) {
-                String bowler = getResultSet.getString(BOWLER_COLUMN);
-                Float economicalRate = Float.parseFloat(getResultSet.getString(ECONOMICAL_RATE));
-                System.out.printf("%-30s %.2f\n" ,bowler,economicalRate);
+            Main main= new Main();
+            for (String query : QUERY)
+            {
+                main.QuerySolver(query,COLUMN_FIRST[iteratorColumn1++],COLUMN_SECOND[iteratorColumn2++]).forEach((k,v)->System.out.printf("\n %-30s %.5s",k,v));
+                System.out.println("\n\n");
             }
 
         } catch (SQLException e) {
@@ -41,18 +52,5 @@ public class Main {
 
     }
 
-    private static void querySolver(String firstQuery, String seasonColumn, String matchPlayed2, String secondQuery, String totalWins2) throws SQLException {
-        query1(firstQuery, matchPlayed2, seasonColumn);
-        query1(secondQuery, totalWins2, WINNER_COLUMN);
-    }
 
-    private static void query1(String secondQuery, String totalWins2, String winnerColumn) throws SQLException {
-        getResultSet = getStatement.executeQuery(secondQuery);
-        while (getResultSet.next()) {
-            String winner = getResultSet.getString(winnerColumn);
-            String totalWins = getResultSet.getString(totalWins2);
-            System.out.printf("%-30s %s\n" ,winner, totalWins);
-        }
-        System.out.println("\n\n");
-    }
 }
