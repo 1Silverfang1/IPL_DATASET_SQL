19975079504f0338c84aa028abc4827cafc0cdcb
root <root@localhost.localdomain>
2019-11-26 14:34:20 +0530

    version1.6

--- src/com/company/ConstantValues.java
+++ src/com/company/ConstantValues.java
@@ -1,25 +1,22 @@
 package com.company;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 class ConstantValues {
-    static int iteratorColumn1 = 0, iteratorColumn2 = 0;
+    static int iteratorColumn1 = 0, iteratorColumn2 = 0,iterator=0;
     static String jdbcUrlforDatabase = "jdbc:postgresql://localhost:5434/Database_test";
     static String username = "postgres";
     static String password = "toor";
     static Connection getConnection = null;
     static Statement getStatement = null;
     static ResultSet getResultSet = null;
-    static String over="6";
-    public static String year = "'2016'", PRE_YEAR = "'2015'";
+    static String [] year={"2015","2016","2015"};
     static String[] COLUMN_FIRST = {"season", "winner", "bowling_team", "winner", "bowler"};
     static String[] COLUMN_SECOND = {"match_played", "total_wins", "extra_runs", "match_win", "economical_rate"};
-    static String[] QUERY = {"select season, count(id) as match_played from matches group by season order by season",
-            "select winner, count(winner) as total_wins from matches  where winner is not null group by winner order by winner",
-            "select delivery.bowling_team, sum(cast(extra_runs as int) ) as extra_runs from delivery where match_id in" +
-                    " (select matches.id from matches where season = " + year + ") group by bowling_team",
-            "select matches.winner, sum(cast(matches.win_by_runs as int))as match_win from matches where season = " + year + " group by winner order by winner;",
-            "select  delivery.bowler, sum(cast(total_runs as int))/cast((count(delivery.bowler)/"+over+")as float) as economical_rate from delivery inner join matches m on delivery.match_id = m.id and m.season= " + PRE_YEAR + " group by bowler order by economical_rate;"};
+    static String[] QUERY = {"select season, count(id) as match_played from matches group by season order by season;",
+            "select winner, count(winner) as total_wins from matches  where winner is not null group by winner order by winner;",
+            "select bowling_team, sum(cast(extra_runs as int) ) as extra_runs from delivery where match_id in (select matches.id from matches where season = ? ) group by bowling_team;",
+            "select matches.winner, sum(cast(matches.win_by_runs as int))as match_win from matches where season = ? group by winner order by winner;",
+            "select  delivery.bowler, sum(cast(total_runs as int))/cast((count(delivery.bowler)/6)as float) as economical_rate from delivery inner join matches m on delivery.match_id = m.id and m.season=? group by bowler order by economical_rate;"};
+
 }
---
+++ src/com/company/DatasetSQL.java
@@ -0,0 +1,87 @@
+package com.company;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.LinkedHashMap;
+
+import static com.company.ConstantValues.*;
+import static com.company.ConstantValues.getResultSet;
+
+public class DatasetSQL {
+
+    private PreparedStatement establishConnection(String query) throws SQLException
+    {
+        getConnection = DriverManager.getConnection(jdbcUrlforDatabase, username, password);
+        return getConnection.prepareStatement(query);
+    }
+
+    private PreparedStatement establishConnection(String query,String year) throws SQLException
+    {
+        getConnection = DriverManager.getConnection(jdbcUrlforDatabase, username, password);
+        PreparedStatement preparedStatement= getConnection.prepareStatement(query);
+        preparedStatement.setString(1,year);
+        return  preparedStatement;
+    }
+    private LinkedHashMap<String,String> mapAdder(String columnFirst, String columnSecond) throws SQLException {
+        LinkedHashMap<String,String> solvedQuery= new LinkedHashMap<>();
+        while (getResultSet.next())
+        {
+            String firstColumn = getResultSet.getString(columnFirst);
+            String secondColumn = getResultSet.getString(columnSecond);
+            solvedQuery.put(firstColumn,secondColumn);
+        }
+        try {
+            if (getStatement != null) {
+                getStatement.close();
+            }
+            if (getResultSet != null) {
+                getResultSet.close();
+            }
+            if (getConnection != null) {
+                getConnection.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return solvedQuery;
+    }
+    private LinkedHashMap<String,String> QuerySolver(String query,String columnFirst, String columnSecond) throws SQLException {
+        getResultSet= establishConnection(query).executeQuery();
+      DatasetSQL datasetSQL= new DatasetSQL();
+        return datasetSQL.mapAdder(columnFirst,columnSecond);
+    }
+    private LinkedHashMap<String,String> QuerySolver(String query,String columnFirst, String columnSecond,String year) throws SQLException {
+        getResultSet= establishConnection(query,year).executeQuery();
+        DatasetSQL datasetSQL= new DatasetSQL();
+        return datasetSQL.mapAdder(columnFirst,columnSecond);
+    }
+    public static void main(String[] args) {
+
+
+        try {
+
+            DatasetSQL datasetObject= new DatasetSQL();
+
+            datasetObject.QuerySolver(QUERY[iterator++],COLUMN_FIRST[iteratorColumn1++],COLUMN_SECOND[iteratorColumn2++]).forEach((k, v)->System.out.printf("\n %-30s %.5s",k,v));
+            System.out.println("\n\n");
+            datasetObject.QuerySolver(QUERY[iterator++],COLUMN_FIRST[iteratorColumn1++],COLUMN_SECOND[iteratorColumn2++]).forEach((k, v)->System.out.printf("\n %-30s %.5s",k,v));
+            System.out.println("\n\n");
+            datasetObject.QuerySolver(QUERY[iterator++],COLUMN_FIRST[iteratorColumn1++],COLUMN_SECOND[iteratorColumn2++],year[0]).forEach((k, v)->System.out.printf("\n %-30s %.5s",k,v));
+            System.out.println("\n\n");
+            datasetObject.QuerySolver(QUERY[iterator++],COLUMN_FIRST[iteratorColumn1++],COLUMN_SECOND[iteratorColumn2++],year[1]).forEach((k, v)->System.out.printf("\n %-30s %.5s",k,v));
+            System.out.println("\n\n");
+            datasetObject.QuerySolver(QUERY[iterator++],COLUMN_FIRST[iteratorColumn1++],COLUMN_SECOND[iteratorColumn2++],year[2]).forEach((k, v)->System.out.printf("\n %-30s %.5s",k,v));
+            System.out.println("\n\n");
+
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+}
