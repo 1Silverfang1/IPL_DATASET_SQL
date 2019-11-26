08777d9b0d3e77978c317e0d67a0ae9e0e4414ed
root <root@localhost.localdomain>
2019-11-26 09:52:21 +0530

    version1.3

--- src/com/company/ConstantValues.java
+++ src/com/company/ConstantValues.java
@@ -12,6 +12,7 @@ class ConstantValues {
     static Connection getConnection = null;
     static Statement getStatement = null;
     static ResultSet getResultSet = null;
+    static String over="6";
     public static String year = "'2016'", PRE_YEAR = "'2015'";
     static String[] COLUMN_FIRST = {"season", "winner", "bowling_team", "winner", "bowler"};
     static String[] COLUMN_SECOND = {"match_played", "total_wins", "extra_runs", "match_win", "economical_rate"};
@@ -20,5 +21,5 @@ class ConstantValues {
             "select delivery.bowling_team, sum(cast(extra_runs as int) ) as extra_runs from delivery where match_id in" +
                     " (select matches.id from matches where season = " + year + ") group by bowling_team",
             "select matches.winner, sum(cast(matches.win_by_runs as int))as match_win from matches where season = " + year + " group by winner order by winner;",
-            "select  delivery.bowler, sum(cast(total_runs as int))/cast((count(delivery.bowler)/6)as float) as economical_rate from delivery inner join matches m on delivery.match_id = m.id and m.season= " + PRE_YEAR + " group by bowler order by economical_rate;"};
+            "select  delivery.bowler, sum(cast(total_runs as int))/cast((count(delivery.bowler)/"+over+")as float) as economical_rate from delivery inner join matches m on delivery.match_id = m.id and m.season= " + PRE_YEAR + " group by bowler order by economical_rate;"};
 }
